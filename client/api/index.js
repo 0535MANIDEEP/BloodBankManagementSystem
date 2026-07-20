56950e97073febca3d0be2998b948c5d4d6049bd
Manideep Daram <manideepdaram@gmail.com>
2026-07-20 16:29:04 +0530

    fix: set Root Directory to client, merge server deps into client/package.json, move API into client/api/

---
+++ client/api/index.js
@@ -0,0 +1,9 @@
+/**
+ * Vercel Serverless Function entry point.
+ * Wraps the Express app for Vercel's serverless infrastructure.
+ */
+
+const app = require('../../server/app');
+
+// For Vercel serverless: export as a named handler
+module.exports = (req, res) => app(req, res);
